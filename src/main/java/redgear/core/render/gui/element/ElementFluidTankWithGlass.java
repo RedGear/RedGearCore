702b5ef4a76f213989fc74a64896a4eb1d09196a
Enosphorous <enosphorous@gmail.com>
2014-04-20 15:46:28 -0700

    Added a new fluid tank with glass overlay.

---
+++ src/main/java/redgear/core/render/gui/element/ElementFluidTankWithGlass.java
@@ -0,0 +1,27 @@
+package redgear.core.render.gui.element;
+
+import net.minecraftforge.fluids.IFluidTank;
+import redgear.core.render.GuiBase;
+import redgear.core.render.RenderHelper;
+
+public class ElementFluidTankWithGlass extends ElementFluidTank{
+
+	public ElementFluidTankWithGlass(GuiBase gui, int posX, int posY, IFluidTank tank) {
+		super(gui, posX, posY, tank);
+	}
+
+	@Override
+	public void draw() {
+
+		if (!visible) {
+			return;
+		}
+		
+		int amount = getScaled();
+		
+		gui.drawFluid(posX, posY + sizeY - amount, tank.getFluid(), sizeX, amount);
+		RenderHelper.bindTexture(texture);
+		drawTexturedModalRect(posX, posY, 17, 1, sizeX, sizeY);
+		drawTexturedModalRect(posX, posY, 33 + gaugeType * 16, 1, sizeX - 1, sizeY);
+	}
+}
