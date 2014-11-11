154f84bc29c207524d75db70038a4d5c267f28aa
LordBlackhole <LordBlackhole@gmail.com>
2014-11-10 19:15:19 -0600

    Deleted old files.
    We don't need these anymore, they were never used anyway.

--- src/main/java/redgear/core/inventory/FuelSlot.java
+++ src/main/java/redgear/core/inventory/FuelSlot.java
@@ -1,12 +1,12 @@
 package redgear.core.inventory;
 
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
-import redgear.core.tile.TileEntityInventory;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class FuelSlot  extends InvSlot{
 
-	public FuelSlot(TileEntityInventory inventory, int x, int y) {
+	public FuelSlot(IInventory inventory, int x, int y) {
 		super(inventory, x, y);
 		this.setMachineRule(TransferRule.INPUT);
 	}
